1037701f05c58aeb050ae882839dffb743619af4
Minsub Sim <minsubsim@gmail.com>
2017-05-16 21:16:57 +0900

    계속하도록 수정

--- script/nlp_worker.py
+++ script/nlp_worker.py
@@ -8,19 +8,22 @@ class NLPWorker():
 
     @staticmethod
     def work():
-        res = db.query_db('SELECT id, body FROM article WHERE parse_b = 0 LIMIT 1000')
-        print(len(res))
-        for id_val, text in res:
-            print(id_val)
-            db.query_db('UPDATE article SET parse_b = 1 WHERE id = ?', [id_val])
-        db.get_db().commit()
-        for id_val, text in res:
-            corpus = NLPWorker.nlplib.pos(text)
-            sentences = NLPWorker.get_sentence(corpus)
-            for sentence in sentences:
-                print(id_val, len(sentence))
-                db.query_db('INSERT INTO parsed_article (a_id, parsed) values (?, ?)', [id_val, json.dumps(sentence)])
-        db.get_db().commit()
+        while True:
+            res = db.query_db('SELECT id, body FROM article WHERE parse_b = 0 LIMIT 1000')
+            print(len(res))
+            if len(res) == 0:
+                break
+
+            for id_val, text in res:
+                print(id_val)
+                db.query_db('UPDATE article SET parse_b = 1 WHERE id = ?', [id_val])
+            db.get_db().commit()
+            for id_val, text in res:
+                corpus = NLPWorker.nlplib.pos(text)
+                sentences = NLPWorker.get_sentence(corpus)
+                for sentence in sentences:
+                    db.query_db('INSERT INTO parsed_article (a_id, parsed) values (?, ?)', [id_val, json.dumps(sentence)])
+            db.get_db().commit()
 
     @staticmethod
     def get_sentence(corpus):
