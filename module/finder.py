f233489b8ec1be2eecb44efb784edf51ab245882
Minsub Sim <minsubsim@gmail.com>
2017-05-23 22:34:15 +0900

    max sim이 아닌 기준 이상 전부 리턴

--- module/finder.py
+++ module/finder.py
@@ -7,14 +7,14 @@ class Finder:
     SEARCH_QUERY = 'SELECT id, body FROM article WHERE id > ? AND context_b = 1 AND parse_b = 1 ORDER BY id ASC LIMIT 100'
     PARSE_QUERY = 'SELECT id, a_id, parsed FROM parsed_article WHERE a_id = ?'
     CONTEXT_QUERY = 'SELECT context_val FROM context WHERE id = ?'
+    SIM_DEADLINE = 0.5
 
     @staticmethod
     def search(text):
         q = NLPParser.parse(text)
         start_id = -1
 
-        max_sim = -1;
-        max_a_id = None;
+        result = []
         while True:
             res = db.query_db(Finder.SEARCH_QUERY, [start_id])
             if len(res) == 0:
@@ -29,16 +29,16 @@ class Finder:
                 dat = json.loads(js)
                 dat = list(map(lambda x: tuple(x), dat))
                 sim = get_similarity(q, dat)
-                if max_sim < sim:
-                    max_sim = sim
-                    max_a_id = a_id
-        if not max_a_id:
-            return 'NOT FOUND'
+                if sim > Finder.SIM_DEADLINE:
+                    result.append([sim, a_id])
 
-        res = db.query_db(Finder.CONTEXT_QUERY, [max_a_id])
-        ctx_val = res[-1][0]
+        result2 = []
+        for sim, a_id in result:
+            res = db.query_db(Finder.CONTEXT_QUERY, [a_id])
+            ctx_val = res[-1][0]
+            adat = db.query_db('SELECT id, title, body, url FROM article WHERE id = ?', [a_id])
+            res = { 'sim_val': sim, 'context_val': ctx_val }
+            res['id'], res['title'], res['body'], res['url'] = adat[-1]
+            result2.append(res)
 
-        adat = db.query_db('SELECT id, title, body, url FROM article WHERE id = ?', [max_a_id])
-        res = { 'sim_val': max_sim, 'context_val': ctx_val }
-        res['id'], res['title'], res['body'], res['url'] = adat[-1]
-        return res
+        return result2
